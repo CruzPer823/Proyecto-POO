ed9dcea03c39f7c67460b49012250309e0de7590
RogerHdzC <92351154+RogerHdzC@users.noreply.github.com>
2022-06-09 21:49:03 -0500

    Más bonito

--- src/main.cpp
+++ src/main.cpp
@@ -8,28 +8,46 @@ using namespace std;
 
 
 int main(){
-  ifstream archivo(NOMBRE_ARCHIVO);
-  string linea;
-  char delimitador = ',';
-  getline(archivo, linea);
-  string ID,NOMBRE,DURACION,GENERO,CALIFICACION;
-  int calificacion;
-  while (getline(archivo,linea)){
-    stringstream stream(linea);
+  int opcion,opcion2;
+  cout<<"BIENVENIDO AL MENU DE VIDEOS"<<"\n"<<
+  "1-. Desplegar por calificación o género"<<"\n"<<
+  "2-. Desplegar por serie y calificación"<<"\n"<<
+  "3-. Desplegar por por películas y calificación"<<"\n"<<
+  "Escoja una opción: "<< endl;
+  cin>>opcion;
+  if (opcion == 1){
+    ifstream archivo(NOMBRE_ARCHIVO);
+    string linea;
+    char delimitador = ',';
+    getline(archivo, linea);
+    string ID,NOMBRE,DURACION,GENERO,CALIFICACION;
+    int calificacion;
+    while (getline(archivo,linea)){
+      stringstream stream(linea);
 
-    getline(stream,ID,delimitador);
-    getline(stream,NOMBRE,delimitador);
-    getline(stream,DURACION,delimitador);
-    getline(stream,GENERO,delimitador);
-    getline(stream,CALIFICACION,delimitador);
+      getline(stream,ID,delimitador);
+      getline(stream,NOMBRE,delimitador);
+      getline(stream,DURACION,delimitador);
+      getline(stream,GENERO,delimitador);
+      getline(stream,CALIFICACION,delimitador);
 
-    calificacion = stoi(CALIFICACION);
+      calificacion = stoi(CALIFICACION);
+    }
+    cout << "Le gustaría desplegar por género o por calificación"<<"\n"<<
+    "1-. Por género" << "\n" << "2-. Por calificación"<<"\n"<<
+    "Ingresa la opción: "<<endl;
+    cin >> opcion2;
+    if (opcion2==2){
+      double usuarioCalif;
+      cout << "Ingrese la calificacion que desea buscar: "<<endl;
+      cin >> usuarioCalif;
+      if (calificacion > usuarioCalif){
+        Videos video1(ID,NOMBRE,DURACION,GENERO,calificacion);
+        cout<<video1.getNombre()<<endl;
+      }
+}
+      archivo.close();
+
   }
-  if (calificacion > 4){
-    Videos video1(ID,NOMBRE,DURACION,GENERO,calificacion);
-    cout<<video1.getNombre()<<endl;
-  }
-
-  archivo.close();
   return 0;
 }
