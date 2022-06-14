ff79cd9dcc5a3e2436543a0648804e5408f04e14
CruzPer823 <105245863+CruzPer823@users.noreply.github.com>
2022-06-14 03:09:44 -0500

    Update Episodios.h

--- src/Episodios.h
+++ src/Episodios.h
@@ -12,7 +12,6 @@ private:
 public:
   Episodios();
   Episodios(std::string, std::string, std::string, std::string, double, int);
-  Episodios(double);
   std::string getNombre();
   std::string getGen();
   double getCalif();
